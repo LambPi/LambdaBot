c29284907bd967724808ef7c2a42f54d4b9eb82a
Maxim Zhulin <mapote@mapote.com>
2017-01-07 15:48:38 -0800

    parseText changes

--- ircBot.py
+++ ircBot.py
@@ -1,5 +1,6 @@
 import socket
 import random
+import sys
 
 from time import sleep
 from insults import insults
@@ -18,7 +19,7 @@ def parseText(text):
             words = text.split(" ")   #split text into words
             sender = words[0][1:text.find("!")]   #get sender
             command = words[1]  #get command              
-            if (sender in ["OREBuild", "ORESchool", "ORESurvival"]) and (command == "PRIVMSG"): 
+            if (sender == "OREBuild" or sender == "ORESchool" or sender == "ORESurvival") and (command == "PRIVMSG"): 
                 content = words[4:]    #check if going to minecraft
                 player = words[3][4:-2]
             else:
@@ -29,7 +30,7 @@ def parseText(text):
             tokens = []
             if len(content) != 0:
                 tokens = content[0].split("$", 1)
-            if len(tokens) == 2 and tokens[0] == "":
+            if len(tokens) == 2 and (tokens[0] == "" or tokens[0] == "\x0f"):
                 botcommand, content = tokens[1], content[1:]
                 botcommand = botcommand.strip()
             else:
@@ -57,33 +58,41 @@ def insult(sender, player, content):
     if content == []:
         sendMsg(insults[random.randint(0, len(insults)-1)], sender, player)
     else:
-        sendMsg("Usage: >insult", sender, player)
+        sendMsg("Usage: $insult", sender, player)
 def wiki(sender, player, content):
     if len(content) == 1:
         sendMsg("https://en.wikipedia.org/wiki/" + content[0], sender, player)
     else:
-        sendMsg("Usage: >wiki [article]", sender, player)
+        sendMsg("Usage: $wiki [article]", sender, player)
 def help(sender, player, content):
     if content == []:
         sendMsg("Commands: %s" % ", ".join(commands.keys()), sender, player)
     else:
-        sendMsg("Usage: >help", sender, player)
+        sendMsg("Usage: $help", sender, player)
 def chikn(sender, player, content):
     if content == []:
         sendMsg("Bwak", sender, player)
     else:
-        sendMsg("Usage: >chikn", sender, player)
+        sendMsg("Usage: $chikn", sender, player)
 def calc(sender, player, content):
     if content != []:
         expression = ' '.join(content).strip()
-        print expression
+        #print expression
         try:
             result = evaluate(expression)
             sendMsg(str(result), sender, player)
         except Exception, info:
             sendMsg("Exception: %s" %(info), sender, player)
     else:
-        sendMsg("Usage: >calc [expression]", sender, player)
+        sendMsg("Usage: $calc [expression]", sender, player)
+
+def bitly(sender, player, content):
+    if content != []:
+        pass
+
+def scramble(sender, player, content):
+    if content != []:
+        pass
 
 
 def receive(connection):
@@ -109,17 +118,20 @@ commands = {
     "calc": calc
 }
 
+password = sys.argv[1]
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
-print "connecting to: "+server
+print "connecting to: " + server
 irc.connect((server, 6667)) #connects to the server
-irc.send("PASS T00thp@ste\r\n")
-irc.send("USER "+ botnick +" "+ botnick +" "+ botnick + " :LambdaBot\n") #user authentication
+irc.send("PASS " + password + "\r\n")
+irc.send("USER "+ botnick + " " + botnick + " " + botnick + " :LambdaBot\n") #user authentication
 irc.send("NICK "+ botnick +"\n")                            #sets nick
 sleep(5)
 irc.send("JOIN " + channel + " \r\n")        #join the chan
 
 irc.send('PRIVMSG ' + channel + ' :Hello! This Is LambBot. Do $help for commands.\r\n')
+#irc.send('PRIVMSG ' + channel + ' :JXU\'s bot is superior\r\n')
 sleep(2)
+
 
 while True:
     receive(irc)
